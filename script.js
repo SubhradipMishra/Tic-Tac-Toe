e62bdeb2e8ac936fc3454e0aa98b03453446334f
SubhradipMishra <156043205+SubhradipMishra@users.noreply.github.com>
2024-02-22 00:50:10 +0530

    Added JS file for TicTacToe

---
+++ script.js
@@ -0,0 +1,56 @@
+
+// console.dir  used  for print object
+
+
+let heading = document.getElementById("heading") //h2
+
+console.log(heading);
+
+let one = document.getElementsByClassName("one");
+ 
+console.dir(one);
+
+
+let para = document.getElementsByTagName("p");
+console.dir(para)
+
+
+
+// Query Slector
+
+
+let firstEl = document.querySelector("p");  // returns only first element
+
+  console.dir(firstEl);
+
+
+let allEl = document.querySelectorAll("p"); // returns all elements
+
+
+ console.dir(allEl);
+
+
+ // tagname
+
+ let res = document.getElementById("click");
+
+    //console.log(res.tagName);
+
+    //innerText
+
+
+     
+
+ 
+let su = document.querySelector("div");
+
+console.dir(su);
+
+// inner html
+
+
+
+
+// text content 
+
+let he = document.getElementById("heading"); 
